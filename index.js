df4e27cbef87f3e5cd78d46175966fa9885ed1dd
Arpita Roy <arpita.roy.workar@gmail.com>
2023-05-18 13:54:03 +0530

    Add function to the menu icon

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // javascript
+
+document.getElementById("menu-bar").addEventListener("click", function(){
+   document.getElementById("nav-bar").style.display = "block"
+})
+document.getElementById("nav-arrow").addEventListener("click", function () {
+    document.getElementById("nav-bar").style.display = "none"
+})
